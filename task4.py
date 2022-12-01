25612cc5b7a12411e40df4d1f7789f5937a5e9ac
dimetrius2000 <106590746+dimetrius2000@users.noreply.github.com>
2022-12-01 17:52:53 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -2,6 +2,7 @@ import hashlib
 import uuid
 
 
+
 cech_dict = {'https://vk.com/feed': 'f035d6ec3afc92d705faf7b30c14aac88aee0f442d5ced604771c809eee774f461e5d10e9760928adf2c422b06de652e27106e27968812d744c5e4ba790ac16f:e2e4b29cdadd4933af2fa60eedd132e3'}
 
 url = (input("url:"))
